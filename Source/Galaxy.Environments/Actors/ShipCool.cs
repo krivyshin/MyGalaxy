0fb08b66badab26a69ec9202689da8a799613fa6
krivyshin <krivyshin27@yandex.ru>
2015-10-02 19:04:53 +0700

    Наследование от Ship

---
+++ Source/Galaxy.Environments/Actors/ShipCool.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Galaxy.Core.Actors;
+using Galaxy.Core.Environment;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Galaxy.Environments.Actors
+{
+    class ShipCool : Ship
+    {
+        public ShipCool(ILevelInfo info)  : base(info)
+        {
+          Width = 30;
+          Height = Height + 25;
+          ActorType = ActorType.Enemy;
+        }
+        public override void Load()
+        {
+            InitTimer();
+            Load(@"Assets\ship4.png");
+        }
+    }
+}
